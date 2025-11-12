4e65d2607fc92f0fd3cbf501e7224b3333c1498d
Mateus Rodrigues <mateus.rodrigues@corteva.com>
2025-11-12 12:04:22 -0300

    [fix] Cors

--- Api/Program.cs
+++ Api/Program.cs
@@ -68,6 +68,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    //.WithOrigins("http://localhost:3000")
+    .SetIsOriginAllowed(origin => true));
+
 app.UseAuthentication();
 app.UseAuthorization();
 
